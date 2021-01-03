aadea6c3f9b1d8ecd1694984fa339729aa4bead4
T-Moretti <66392558+T-Moretti@users.noreply.github.com>
2021-01-03 08:25:43 -0800

    Update index.js

--- index.js
+++ index.js
@@ -15,15 +15,14 @@ function formatQueryParams(params) {
 
 // inserts recipe instructions
 function insertInstructions(id, responseJson) {
-    console.log(id);
-    console.log(responseJson);
     let instructionsArray = responseJson; 
-    $(`#${id}`).append(`<h3>Instructions</h3><ul>`)
+    let htmlToInsert = "<h3>Instructions</h3><ul>";   
     for (let i = 0; i < instructionsArray[0].steps.length; i++) {
-        $(`#${id}`).append(`<li>${instructionsArray[0].steps[i].step}</li>`) 
+        htmlToInsert = htmlToInsert + `<li>${instructionsArray[0].steps[i].step}</li>`;
     }
-    $(`#${id}`).append(`</ul>`)
-};
+    htmlToInsert = htmlToInsert + `</ul>`; 
+    $(`#${id}`).append(htmlToInsert);
+}
 
 // grabs recipe instructions
 function findRecipeInstructions(id) {
@@ -33,7 +32,7 @@ function findRecipeInstructions(id) {
     const queryString = formatQueryParams(params);
     const instructionsURL = `https://api.spoonacular.com/recipes/${id}/analyzedInstructions`;
     const instructionsURLWithParams = instructionsURL + '?' + queryString;
-
+   
     fetch(instructionsURLWithParams)
         .then(response => {
             if (response.ok) {
@@ -47,19 +46,18 @@ function findRecipeInstructions(id) {
         .catch(error => {
             $('#js-error-message').text(`Something went wrong: ${error.message}`)
         });
-};
+}
 
 // inserts recipe ingredients 
 function insertIngredients(id, responseJson) {
-    console.log(id);
-    console.log(responseJson);
     let ingredientsArray = responseJson.ingredients;
-    $(`#${id}`).append(`<h3>Ingredients</h3><ul>`)
+    let htmlToInsert = "<h3>Ingredients</h3><ul>";   
     for (let i = 0; i < ingredientsArray.length; i++) {
-        $(`#${id}`).append(`<li>${ingredientsArray[i].name} - ${ingredientsArray[i].amount.us.value} ${ingredientsArray[i].amount.us.unit}</li>`);
+        htmlToInsert = htmlToInsert + `<li>${ingredientsArray[i].name} - ${ingredientsArray[i].amount.us.value} ${ingredientsArray[i].amount.us.unit}</li>`;
     }
-    $(`#${id}`).append(`</ul>`)
-};
+    htmlToInsert = htmlToInsert + `</ul>`; 
+    $(`#${id}`).append(htmlToInsert);
+}
 
 // grabs recipe ingredients 
 function findRecipeIngredients(id) { 
@@ -84,23 +82,27 @@ function findRecipeIngredients(id) {
         .catch(error => {
             $('#js-error-message').text(`Something went wrong: ${error.message}`)
         });
-};
+}
 
 
 // currently displays all results - There are two other functions, one called at the 
 // bottom here, the other within that one
 function displayResults(responseJson) {
     $('#results').empty();
-    for (let i = 0; i < responseJson.results.length; i++) {
-      
-        let image = responseJson.results[i].image;
-        let recipeID = responseJson.results[i].id;
-        $('#results').append(`<div id=${recipeID}><h2>${responseJson.results[i].title}</h2>
-        <img src='${image}' alt='Recipe Picture'></div>`);
-          
-        findRecipeIngredients(recipeID);
-        
-        $('.results').removeClass('hidden');
+    $('#js-error-message').empty();
+    if (responseJson.results.length === 0) {
+        $('#js-error-message').text(`Oops couldn't find any recipes based on your search. Please try again.`);
+    } else {
+        for (let i = 0; i < responseJson.results.length; i++) {
+            let image = responseJson.results[i].image;
+            let recipeID = responseJson.results[i].id;
+            $('#results').append(`<div id=${recipeID} class='result'><h2>${responseJson.results[i].title}</h2>
+            <img src='${image}' alt='Recipe Picture'></div>`);
+              
+            findRecipeIngredients(recipeID);
+            
+            $('.results').removeClass('hidden');
+        }
     }
 }
 
@@ -127,16 +129,16 @@ function findRecipeResults(searchTerm) {
         .catch(error => {
             $('#js-error-message').text(`Something went wrong: ${error.message}`)
         });
-};
+}
 
 // runs the submit event, then calls findRecipeResults function
 function watchForm() {
     $('form').submit(event => {
         event.preventDefault();
         const searchTerm = $('#js-search-term').val();
-        findRecipeResults(searchTerm);
-    });
-};
+        findRecipeResults(searchTerm); 
+    })
+}
 
 // starting point
 $(watchForm);
